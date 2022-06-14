5a100004aa6d53550fab1df08ae771f658551e92
nizmkm <nizammusthafa815@gmail.com>
2022-06-14 10:16:02 +0530

    sum

---
+++ Nizam/list.py
@@ -0,0 +1,17 @@
+my_tuple=(1,2,3,4,5,6)
+print(my_tuple)
+
+for i in my_tuple:
+    print(i)
+
+my_list=[1,2,3,4,5,6]
+print(my_list)
+
+for i in my_list:
+    print(i)
+
+my_set={1,2,3,4,5,6}
+print(my_set)
+
+for i in my_set:
+    print(i)
---
+++ Nizam/sum.py
@@ -0,0 +1,2 @@
+my_list=[2,4,6,8,10]
+print(sum(my_list))
