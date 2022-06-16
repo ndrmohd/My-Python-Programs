612fd8a20016f24323158212e689be31e0db40e3
nizmkm <nizammusthafa815@gmail.com>
2022-06-16 10:30:57 +0530

    list

--- Nizam/list.py
+++ Nizam/list.py
@@ -1,17 +1,3 @@
-my_tuple=(1,2,3,4,5,6)
-print(my_tuple)
-
-for i in my_tuple:
-    print(i)
-
-my_list=[1,2,3,4,5,6]
-print(my_list)
-
-for i in my_list:
-    print(i)
-
-my_set={1,2,3,4,5,6}
-print(my_set)
-
-for i in my_set:
-    print(i)
+a=(input("Enter the numbers"))
+b=a.split()
+print(b)
