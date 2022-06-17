866909ab2eb3f1a436dce3770b1c65a5249fe9ef
ndrmohd <mohdndr188@gmail.com>
2022-06-17 10:19:10 +0530

    dot decends

---
+++ Nadir/leap.py
@@ -0,0 +1,12 @@
+from doctest import ELLIPSIS_MARKER
+
+
+month=str(input("enter month"))
+year=int(input("enter year"))
+
+if year%400==0 and year%100==0
+    print("leap year")
+elif year%4==0 and year%100!=0
+    print("leap year")
+else:
+    print("not leap year")    
