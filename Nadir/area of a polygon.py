fe4fdddd42dc93c6a4477fe00ba8a8b7ffae9e8a
ndrmohd <mohdndr188@gmail.com>
2022-06-20 11:27:35 +0530

    20/06/2022

---
+++ Nadir/area of a polygon.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the number of sides of the polygon= "))
+if a==0:
+    r=int(input('Enter the radius of the circle= '))
+    area=3.14(r*r)
+    print(area)
+elif a>=3:
+    b=int(input('Enter the length of one side of the polygon= '))
+    ri=
+    area=(a*b*ri)/2
+    print(area)
+else:
+    print(0)
