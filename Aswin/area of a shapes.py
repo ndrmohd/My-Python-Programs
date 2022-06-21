d81fb7bbac1427378a63bc2eb1a6b47ad20c752e
aswin kandanakam <aswinvkandnakam@gmail.com>
2022-06-21 10:25:03 +0530

    21/06/2022

---
+++ Aswin/area of a shapes.py
@@ -0,0 +1,18 @@
+from math import sqrt, tan
+a=int(input("Enter the number of sides of the polygon= "))
+if a==0:
+    r=int(input('Enter the radius of the circle= '))
+    area=3.14*r*r
+    print(area)
+elif a==3:
+    c=int(input('Enter the length of one side of triangle= '))
+    s=(3*c)/2
+    area=sqrt(s*((s-c)*(s-c)*(s-c)))
+    print(area)
+elif a==4:
+    l=int(input('Enter the length of the quadrilateral= '))
+    b=int(input('Enter the breadth of the quadrilateral= '))
+    area=l*b
+    print(area)
+else:
+    print("There is no polygon with sides",a)
