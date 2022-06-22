f80e4767c64ba47cd726b345c2ab6aa88d2dc598
aswin kandanakam <aswinvkandnakam@gmail.com>
2022-06-22 10:41:47 +0530

    scientific calculator

--- Aswin/prime.py
+++ Aswin/prime.py
@@ -1,8 +1,8 @@
-a=int(input("enter the range"))
-for i in range(2,a+1):
-    if (a%i)==0:              
-           print('')
-
-
-
-    
+a=int(input("enter the number"))
+for i in range(2,a):
+    if (a%i)==0:
+        print(a,"is not a prime number")
+        print(i,"times",a//i,"is",a)
+        break
+else:
+       print(a,"is a prime number")
