230379d6d0162bd31b64f4f92b09f08b42464aae
aswin kandanakam <aswinvkandnakam@gmail.com>
2022-06-22 11:26:18 +0530

    calculator

--- Aswin/calculator.py
+++ Aswin/calculator.py
@@ -1,41 +1,49 @@
 from math import cos, sin, sqrt, tan
+from re import X
 print("1.ADDITION\n2.SUBTRACTION\n3.DIVISION\n4.MULTIPLIACTION\n5.SQURE\n6.SQURE ROOT\n7.CUBE\n8.SIN\n9.COS\n10.TAN\n11.FLOOR DIVISION\n12.EXIT")
-a=int(input("Select Your Choice"))
-if a==1:
+x=int(input("Select Your Choice"))
+while X==1:
     a=int(input('Enter the first number='))
     b=int(input('Enter the second number='))
     print(a+b)
-elif a==2:
+if x==2:
     a=int(input('Enter the first number='))
     b=int(input('Enter the second number='))
     print(a-b)
-elif a==3:
+elif x==3:
     a=int(input('Enter the first number='))
     b=int(input('Enter the second number='))
     print(a/b)
-elif a==4:
+elif x==4:
     a=int(input('Enter the first number='))
     b=int(input('Enter the second number='))
     print(a*b)
-elif a==5:
+elif x==5:
     a=int(input('Enter the  number='))
     print(a*a)
-elif a==6:
+elif x==6:
     a=int(input('Enter the number='))
     print(sqrt(a))
-elif a==7:
+elif x==7:
     a=int(input('Enter the number='))
     print(a*a*a)
-elif a==8:
+elif x==8:
     a=int(input('Enter the number='))
     print(sin(a))
-elif a==9:
+elif x==9:
     a=int(input('Enter the number='))
     print(cos(a))
-elif a==10:
+elif x==10:
     a=int(input('Enter the number='))
     print(tan(a))
-elif a==11:
+elif x==11:
     a=int(input('Enter the first number='))
     b=int(input('Enter the second number='))
     print(a//b)
+elif x==12:
+    a=("exited")
+    print(a)
+else:
+    print(x)
+
+    
