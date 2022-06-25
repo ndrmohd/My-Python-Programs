f5f0cfc499620d8935f6f8114bc1e5c22509879d
nizmkm <nizammusthafa815@gmail.com>
2022-06-25 11:27:40 +0530

    25-06-2022

---
+++ Nizam/balancedstring.py
@@ -0,0 +1,12 @@
+from itertools import count
+string=input("Enter string:")
+count=0
+s=0
+for i in string:
+ if i=='a' or i=='A':
+    count=count+1
+ elif i=='b' or i=='B':
+    count=count-1
+ if count==0:
+    s=s+1
+print(s)
