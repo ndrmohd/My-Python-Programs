5b13c95a43a1c69190b7a807b047f9e8afa9fc16
aswin kandanakam <aswinvkandnakam@gmail.com>
2022-06-25 11:23:34 +0530

    25-06-2022

---
+++ Aswin/string count.py
@@ -0,0 +1,11 @@
+string=input("enter a string: ")
+count=0
+s=0
+for i in string:
+ if i=='a' or i=='A':
+    count=count+1
+ if i=='b' or i=='B':
+    count=count-1
+ if count==0:
+    s+=1
+print(s)
