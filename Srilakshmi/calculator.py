8d53f6f842674c78e130ff3972becb73955138c5
srilakshmi <srilakshmiprakash9@gmail.com>
2022-06-28 11:28:30 +0530

    28/6/22

--- Srilakshmi/calculator.py
+++ Srilakshmi/calculator.py
@@ -5,9 +5,7 @@ while login=="y" or login=="Y":
         print('1-add \n 2-substraction \n 3-multiplication \n 4-division \n 5-floor division \n 6-sin \n 7-cos \n 8-tan \n 9-power \n 10- square root')
         function=input("select the function")
         if function=="1" or function=="add":
-            num1=int(input("enter the number"))
-            num2=int(input("enter the number"))
-            print(num1+num2)
+            sum()
         elif function=="2" or function=='substraction':
             num1s=int(input("enter the number"))
             num2s=int(input("enter the number"))
