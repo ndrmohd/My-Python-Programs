884dca5f2513b20d1806f38cfda6ad6d8ac3cd7f
aswin kandanakam <aswinvkandnakam@gmail.com>
2022-07-01 11:31:43 +0530

    1-07-2022

---
+++ Aswin/circle.py
@@ -0,0 +1,7 @@
+from turtle import*
+import turtle
+for i in range(90):
+    for j in range(4):
+        turtle.fd(100)
+        turtle.rt(90)
+    turtle.rt(10)
---
+++ Aswin/star.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.fd(200)
+turtle.rt(144)
+turtle.fd(200)
+turtle.rt(144)
+turtle.fd(200)
+turtle.rt(144)
+turtle.fd(200)
+turtle.rt(144)
+turtle.fd(200)
+
