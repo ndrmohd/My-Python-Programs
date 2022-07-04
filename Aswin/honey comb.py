67926c064e739e21966d3f3b4e2725fd6297021d
aswin kandanakam <aswinvkandnakam@gmail.com>
2022-07-04 11:29:44 +0530

    honey comb

---
+++ Aswin/honey comb.py
@@ -0,0 +1,14 @@
+import turtle
+for i in range(3):
+    for j in range(6):
+        turtle.fd(60)
+        turtle.rt(60)
+    turtle.rt(120)
+turtle.fd(60)
+turtle.lt(60)
+for i in range(4):
+    for j in range(5):
+        turtle.fd(60)
+        turtle.rt(60)
+    turtle.rt(120)
+
