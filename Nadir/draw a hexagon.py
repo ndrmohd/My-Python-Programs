8abc4ad25058a02afdbbe64a92625c05a8b16a03
ndrmohd <mohdndr188@gmailcom>
2022-07-04 11:28:46 +0530

    04/07/2022

--- Nadir/draw a hexagon.py
+++ Nadir/draw a hexagon.py
@@ -1,13 +1,4 @@
 import turtle
-for i in range(3):
-    for j in range(6):
-        turtle.fd(50)
-        turtle.rt(60)
-    turtle.rt(120)
-turtle.fd(50)
-turtle.rt(60)
-for i in range(3):
-    for j in range(5):
-        turtle.fd(50)
-        turtle.lt(60)
-    turtle.lt(120)
+for i in range(6):
+    turtle.fd(100)
+    turtle.rt(60)
