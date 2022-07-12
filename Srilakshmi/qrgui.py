3882134c61e37c487654c2d64a27bf7bb61ff5e1
srilakshmi <srilakshmiprakash9@gmail.com>
2022-07-12 12:11:50 +0530

    12/7/22

--- Srilakshmi/qrgui.py
+++ Srilakshmi/qrgui.py
@@ -1,12 +1,17 @@
 from tkinter import *
 import qrcode
 window=Tk()
-l1=Label(window,text="Name ").grid(row=0)
+def qr():
+    name=e1.get()
+    age=e2.get()
+    details=name+age
+    mycode=qrcode.make(details)
+    mycode.save("code2.png")
+l1=Label(window,text="Name").grid(row=0)
 l2=Label(window,text="age").grid(row=1)
-e1=Entry(window,).grid(row=0,column=1)
-e2=Entry(window,).grid(row=1,column=1)
-bt=Button(text="Generate qrcode").grid(row=2)
-details=e1.get(),e2.get()
-mycode=qrcode.make(details)
-mycode.save("code2.png")
+e1=Entry(window)
+e1.grid(row=0,column=1)
+e2=Entry(window)
+e2.grid(row=1,column=1)
+bt=Button(text="Generate qrcode",command=qr).grid(row=2)
 window.mainloop()
